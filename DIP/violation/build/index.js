8650a49b65c561a1f2473acc5ae9cafc2816646e
ahmad.shoja.999 <ahmad.shoja.999@gmail.com>
2023-03-18 01:58:43 +0330

    [Clean] remove unwanted build folders

--- DIP/violation/build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TimeService_1 = require("./TimeService");
-let timeService = new TimeService_1.TimeService();
-timeService.start();
-setTimeout(() => { timeService.stop(); }, 10000);
-//# sourceMappingURL=index.js.map
